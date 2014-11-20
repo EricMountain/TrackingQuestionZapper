980e28c43e3a63f8acce85bd057c8ed85f00498d
Eric Mountain <eric.stephen.mountain@gmail.com>
2014-11-20 21:32:46 +0100

    Add youtube and lemonde.fr.  Refactor.

--- Tracking_Question_Zapper.user.js
+++ Tracking_Question_Zapper.user.js
@@ -7,12 +7,17 @@
 // @include     https://www.google.tld/*
 // @include     http://slashdot.org/*
 // @include     http://*.slashdot.org/*
+// @include     http*://youtube.com/*
+// @include     http*://*.youtube.com/*
+// @include     http*://lemonde.fr/*
+// @include     http*://*.lemonde.fr/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 // From http://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
-function eventFire(el, etype){
+// Fires the specified event type ('click'...) on the given element.
+function eventFire(el, etype) {
   if (el.fireEvent) {
     (el.fireEvent('on' + etype));
   } else {
@@ -22,34 +27,58 @@ function eventFire(el, etype){
   }
 }
 
-function google() {
-    var okButton = document.querySelector("#epb-ok");
-    if (typeof okButton !== 'undefined') {
-        eventFire(okButton, 'click');
+// Gets the specified element from the DOM, and fires a click event on it.
+// Returns false if no element matched, true otherwise.
+function zapQuestion(selector) {
+    var element = document.querySelector(selector);
+
+    if (typeof element === 'undefined') {
+        console.log("No matching element found in document.");
+        return false;
+    } else {
+        console.log("Matching element found in document.");
+        eventFire(element, 'click');
+        return true;
     }
 }
 
+function google() {
+    return zapQuestion("#epb-ok");
+}
+
 function slashdot() {
-    var okButton = document.querySelector("div.container > div.textcont > p > a.btn");
-    if (typeof okButton !== 'undefined') {
-        eventFire(okButton, 'click');
-    }
+    return zapQuestion("div.container > div.textcont > p > a.btn");
+}
+
+function youtube() {
+    return zapQuestion("div.cookie-alert > div.yt-alert-buttons > button.yt-uix-button");
+}
+
+function lemonde() {
+    return zapQuestion("div#alerte_tracking > a:nth-child(1)");
 }
 
 var knownSites = [
     { r: /^https?:\/\/.*google\..*/, f: google},
-    { r: /^https?:\/\/.*slashdot\..*/, f: slashdot}
+    { r: /^https?:\/\/.*google\..*/, f: youtube},
+    { r: /^https?:\/\/.*slashdot\..*/, f: slashdot},
+    { r: /^https?:\/\/.*youtube\..*/, f: youtube },
+    { r: /^https?:\/\/.*lemonde\.fr/, f: lemonde }
 ];
 
 knownSites.every(function(element, index, array) {
     console.log('Test:', location.href, element, index);
     if (element.r.test(location.href)) {
-        console.log('Matched.');
-        element.f();
-        return false;
+        console.log('URL matched rule.');
+
+        // Some sites (e.g. Youtube using Google accounts) match one
+        // URL, but need the question handling of another site.  So we
+        // don't stop trying to match unless the selector itself matches
+        // an element of the document.
+
+        return !element.f();
     } else {
-        console.log('Didn\'t match.');
+        console.log('URL didn\'t match rule.');
         return true;
     }
 });
-
